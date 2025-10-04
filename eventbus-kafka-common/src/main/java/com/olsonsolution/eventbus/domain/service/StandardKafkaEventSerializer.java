9883f395e19e8c56d6b5f91317e3d401ffd120a4
olson1998 <aleksander.inglot98@gmail.com>
2025-10-04 09:12:56 +0200

    Enrich error handling

--- eventbus-kafka-common/src/main/java/com/olsonsolution/eventbus/domain/service/StandardKafkaEventSerializer.java
+++ eventbus-kafka-common/src/main/java/com/olsonsolution/eventbus/domain/service/StandardKafkaEventSerializer.java
@@ -1,10 +1,12 @@
 package com.olsonsolution.eventbus.domain.service;
 
 import com.asyncapi.v3._0_0.model.AsyncAPI;
+import com.olsonsolution.eventbus.domain.model.kafka.exception.KafkaSerializationException;
 import com.olsonsolution.eventbus.domain.port.repository.EventMapper;
 import com.olsonsolution.eventbus.domain.port.repository.KafkaEventSerializer;
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
+
+import java.io.IOException;
 
 @RequiredArgsConstructor
 public class StandardKafkaEventSerializer<T> implements KafkaEventSerializer<T> {
@@ -14,9 +16,12 @@ public class StandardKafkaEventSerializer<T> implements KafkaEventSerializer<T> 
     private final EventMapper<T> eventMapper;
 
     @Override
-    @SneakyThrows
     public byte[] serialize(String s, T t) {
-        return eventMapper.writeValue(s, t, apiDocs);
+        try {
+            return eventMapper.writeValue(s, t, apiDocs);
+        } catch (IOException e) {
+            throw new KafkaSerializationException(e);
+        }
     }
 
 }
